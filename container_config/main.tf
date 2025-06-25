a0c037fde8eed46d327863668e839212b45a6125
hardworker635 <2397815@cognizant.com>
2025-06-25 15:46:31 +0530

    removed role allocation

--- container_config/main.tf
+++ container_config/main.tf
@@ -50,9 +50,3 @@ resource "azurerm_kubernetes_cluster" "dev_aks" {
 
 
 }
-resource "azurerm_role_assignment" "example" {
-  principal_id                     = azurerm_kubernetes_cluster.dev_aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.dev_acr.id
-  skip_service_principal_aad_check = true
-}
