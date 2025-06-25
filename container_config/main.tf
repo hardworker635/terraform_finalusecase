8838419bf289b1e54ac80a697f5b66c9fe73b0e0
hardworker635 <2397815@cognizant.com>
2025-06-25 14:59:13 +0530

    added kubernetes infra

--- container_config/main.tf
+++ container_config/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.0.0"
+      version = "~>4.0.0"
       
     }
   }
@@ -28,41 +28,31 @@ resource "azurerm_container_registry" "dev_acr" {
   resource_group_name = azurerm_resource_group.dev_rg.name
   location            = "East US"
   sku                 = "Basic"
+  
 
   }
 
-resource "azurerm_container_registry_scope_map" "dev_acr_map" {
-    name = "devacrscopemap"
-    container_registry_name = azurerm_container_registry.dev_acr.name
-    resource_group_name = azurerm_resource_group.dev_rg.name
-    actions = [
-    "repositories/*/content/read",
-    "repositories/*/content/write"
-  ] # This gives the token permission to pull/push to all repositories. You can customize this per repo if needed.
-}
+resource "azurerm_kubernetes_cluster" "dev_aks" {
+  name                = "dev_AKS"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.dev_rg.name
+  dns_prefix          = "devaks"
+  sku_tier = "Free"
 
-resource "azurerm_container_registry_token" "dev_acr_token" {
-  name                    = "devacrtoken"
-  container_registry_name = azurerm_container_registry.dev_acr.name
-  resource_group_name     = azurerm_resource_group.dev_rg.name
-  scope_map_id            = azurerm_container_registry_scope_map.dev_acr_map.id
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
 
-}
-
-resource "azurerm_container_registry_token_password" "dev_acr_token_pswd" {
-  container_registry_token_id = azurerm_container_registry_token.dev_acr_token.id
-
-  password1 {
-   
-  } 
 
 }
-
-#output "acr_token_username" {
- # value = azurerm_container_registry_token.dev_acr_token.name
-#}
-
-output "acr_token_password" {
-  value     = azurerm_container_registry_token_password.dev_acr_token_pswd.password1[0]
-  sensitive = true
+resource "azurerm_role_assignment" "example" {
+  principal_id                     = azurerm_kubernetes_cluster.dev_aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.dev_acr.id
+  skip_service_principal_aad_check = true
 }
